5016ad564400aedfeacc1d6619b75a31179e3423
Rain <hramil0120@gmail.com>
2020-03-25 12:54:44 +0300

    Add getters and setters to FlatOptions

--- src/main/java/lab5/storage/FlatOptions.java
+++ src/main/java/lab5/storage/FlatOptions.java
@@ -34,4 +34,84 @@ public class FlatOptions {
     this.houseYear = houseYear;
     this.coordinates = coordinates;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getArea() {
+    return area;
+  }
+
+  public Integer getNumberOfRooms() {
+    return numberOfRooms;
+  }
+
+  public boolean isBalcony() {
+    return balcony;
+  }
+
+  public double getTimeToMetroByTransport() {
+    return timeToMetroByTransport;
+  }
+
+  public View getView() {
+    return view;
+  }
+
+  public String getHouseName() {
+    return houseName;
+  }
+
+  public Coordinates getCoordinates() {
+    return coordinates;
+  }
+
+  public IDGenerator<Integer> getIdGenerator() {
+    return idGenerator;
+  }
+
+  public Integer getHouseYear() {
+    return houseYear;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setArea(int area) {
+    this.area = area;
+  }
+
+  public void setNumberOfRooms(Integer numberOfRooms) {
+    this.numberOfRooms = numberOfRooms;
+  }
+
+  public void setBalcony(boolean balcony) {
+    this.balcony = balcony;
+  }
+
+  public void setTimeToMetroByTransport(double timeToMetroByTransport) {
+    this.timeToMetroByTransport = timeToMetroByTransport;
+  }
+
+  public void setView(View view) {
+    this.view = view;
+  }
+
+  public void setHouseName(String houseName) {
+    this.houseName = houseName;
+  }
+
+  public void setCoordinates(Coordinates coordinates) {
+    this.coordinates = coordinates;
+  }
+
+  public void setIdGenerator(IDGenerator<Integer> idGenerator) {
+    this.idGenerator = idGenerator;
+  }
+
+  public void setHouseYear(Integer houseYear) {
+    this.houseYear = houseYear;
+  }
 }
