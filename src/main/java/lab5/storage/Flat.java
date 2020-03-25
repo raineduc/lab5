12aa5d9bea4d99e6a0968219bf1e996203fa826f
Rain <hramil0120@gmail.com>
2020-03-25 17:11:01 +0300

    Delete constructors with long parameter list in Flat and FlatOptions; Change fields' access modifiers to private in FlatOptions

--- src/main/java/lab5/storage/Flat.java
+++ src/main/java/lab5/storage/Flat.java
@@ -1,52 +1,15 @@
 package lab5.storage;
 
 import lab5.lib.ValidationException;
-import lab5.storage.jaxb_adapters.FlatsAdapter;
 import lab5.utils.IDGenerator;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.LocalDateTime;
 
 public class Flat implements Comparable<Flat>  {
   private int id;
-  private String name;
-  private Coordinates coordinates;
   private LocalDateTime creationDate = LocalDateTime.now();
-  private int area;
-  private Integer numberOfRooms; //Максимальное значение поля: 11, Значение поля должно быть больше 0
-  private boolean balcony;
-  private double timeToMetroByTransport; //Значение поля должно быть больше 0
-  private View view; //Поле может быть null
   private House house; //Поле не может быть null
   private FlatOptions options;
-
-  public Flat(String name,
-              int area,
-              Integer numberOfRooms,
-              boolean balcony,
-              double timeToMetroByTransport,
-              View view,
-              String houseName,
-              Integer houseYear,
-              Coordinates coordinates,
-              IDGenerator<Integer> idGenerator) throws ValidationException, NullPointerException {
-    this.house = new House(houseName, houseYear);
-
-    FlatValidator.validate(name, coordinates, area, view, numberOfRooms, timeToMetroByTransport, house);
-
-    this.id = idGenerator.generate();
-    this.name = name;
-    this.area = area;
-    this.numberOfRooms = numberOfRooms;
-    this.balcony = balcony;
-    this.timeToMetroByTransport = timeToMetroByTransport;
-    this.view = view;
-    this.coordinates = coordinates;
-    this.house = new House(houseName, houseYear);
-  }
 
   public Flat(FlatOptions options, IDGenerator<Integer> idGenerator) throws ValidationException, NullPointerException {
     this.id = idGenerator.generate();
@@ -60,7 +23,7 @@ public class Flat implements Comparable<Flat>  {
   }
 
   public View getView() {
-    return view;
+    return options.getView();
   }
 
   public int getId() {
@@ -68,11 +31,11 @@ public class Flat implements Comparable<Flat>  {
   }
 
   public Integer getNumberOfRooms() {
-    return numberOfRooms;
+    return options.getNumberOfRooms();
   }
 
   public double getTimeToMetroByTransport() {
-    return timeToMetroByTransport;
+    return options.getTimeToMetroByTransport();
   }
 
   public int compareTo(Flat flat) {
@@ -80,19 +43,19 @@ public class Flat implements Comparable<Flat>  {
   }
 
   public int getArea() {
-    return area;
+    return options.getArea();
   }
 
   public String getName() {
-    return name;
+    return options.getName();
   }
 
   public Coordinates getCoordinates() {
-    return coordinates;
+    return options.getCoordinates();
   }
 
   public boolean isBalcony() {
-    return balcony;
+    return options.isBalcony();
   }
 
   public House getHouse() {
@@ -101,15 +64,15 @@ public class Flat implements Comparable<Flat>  {
 
   public String toString() {
     return "{\n" +
-            "  name: " + name + "\n" +
-            "  view: " + view.toString() + "\n" +
+            "  name: " + getName() + "\n" +
+            "  view: " + getView().toString() + "\n" +
             "  id: " + id + "\n" +
-            "  number of rooms: " + numberOfRooms + "\n" +
-            "  time to metro by transport: " + timeToMetroByTransport + "\n" +
-            "  area: " + area + "\n" +
-            "  balcony: " + balcony + "\n" +
+            "  number of rooms: " + getNumberOfRooms() + "\n" +
+            "  time to metro by transport: " + getTimeToMetroByTransport() + "\n" +
+            "  area: " + getArea() + "\n" +
+            "  balcony: " + isBalcony() + "\n" +
             "  house:\n" + house.toString(2) + "\n" +
-            "  coordinates:\n" + coordinates.toString(2) + "\n" +
+            "  coordinates:\n" + getCoordinates().toString(2) + "\n" +
             "}";
   }
 }
--- src/main/java/lab5/storage/FlatOptions.java
+++ src/main/java/lab5/storage/FlatOptions.java
@@ -1,42 +1,15 @@
 package lab5.storage;
 
-import lab5.utils.IDGenerator;
-
 public class FlatOptions {
-  public String name;
-  public int area;
-  public Integer numberOfRooms;
-  public boolean balcony;
-  public double timeToMetroByTransport;
-  public View view;
-  public String houseName;
-  public Coordinates coordinates;
-  public Integer houseYear;
-
-  public FlatOptions(String name,
-                     int area,
-                     Integer numberOfRooms,
-                     boolean balcony,
-                     double timeToMetroByTransport,
-                     View view,
-                     String houseName,
-                     Integer houseYear,
-                     Coordinates coordinates
-  ) {
-    this.name = name;
-    this.area = area;
-    this.numberOfRooms = numberOfRooms;
-    this.balcony = balcony;
-    this.timeToMetroByTransport = timeToMetroByTransport;
-    this.view = view;
-    this.houseName = houseName;
-    this.houseYear = houseYear;
-    this.coordinates = coordinates;
-  }
-
-  public FlatOptions() {
-    super();
-  }
+  private String name;
+  private int area;
+  private Integer numberOfRooms;
+  private boolean balcony;
+  private double timeToMetroByTransport;
+  private View view;
+  private String houseName;
+  private Coordinates coordinates;
+  private Integer houseYear;
 
   public String getName() {
     return name;
